306cc24efe83c4e1aac697426fa24fd879ab839a
Vinicius Saraiva <chagas.vis@gmail.com>
2022-07-31 19:37:55 -0300

    feat: embeddedServer

--- src/main/kotlin/org/uatu/Application.kt
+++ src/main/kotlin/org/uatu/Application.kt
@@ -2,20 +2,19 @@ package org.uatu
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.netty.EngineMain
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import org.uatu.plugins.configureRouting
 import org.uatu.plugins.configureSerialization
 import org.koin.ktor.plugin.Koin
 import org.uatu.modules.DagModule
 
-fun main(args: Array<String>): Unit =
-    EngineMain.main(args)
-
-@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
-fun Application.module() {
-    configureRouting()
-    configureSerialization()
-    install (Koin){
-        modules(DagModule)
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = 8888) {
+        configureRouting()
+        configureSerialization()
+        install (Koin){
+            modules(DagModule)
     }
+    }.start(wait = true)
 }
